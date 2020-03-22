d9f48a7c950f30630f60f4e9e9cbb651b0ffc269
Denis <Danilkin58@gmail.com>
2020-03-22 23:45:13 +0300

    feat: add first idea about calculator algorithm

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let regexp = /\([0-9\+\-\*\/\s]+\)/ig;
+    let arr = [];
+    let result = 0;
+    if(~expr.indexOf("("))
+        arr = regexp.exec(expr);
+    if(arr.length == 0) {
+        result = calculate(expr);
+    }
+    return result;
+}
+
+function calculate (str) {
+    let symb = /[\+\-\*\/]/;
+    let bufferArr = str.split(symb);
+    while(bufferArr.length != 1) {        
+       
+    }
 }
 
 module.exports = {
